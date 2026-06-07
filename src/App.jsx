747067f2adba2c99d7fb835534b3a5e102aa9dfe
Kenny Le <kennyle281@gmail.com>
2026-06-07 10:55:35 -0400

    A1 home portfolio (#5)
    * code refractor
    
    * added linking to barbershop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
-import BikeRepair from './page-holders/BikeRepair'
 import MemoryGame from './page-holders/MemoryGame'
 import EcommerceStore from './page-holders/EcommerceStore'
 import AnalyticsDashboard from './page-holders/AnalyticsDashboard'
@@ -13,7 +12,6 @@ function App() {
       <Route path="/" element={<Home />} />
 
       {/* Project placeholder pages */}
-      <Route path="/bike-repair" element={<BikeRepair />} />
       <Route path="/memory-game" element={<MemoryGame />} />
       <Route path="/ecommerce" element={<EcommerceStore />} />
       <Route path="/analytics" element={<AnalyticsDashboard />} />
